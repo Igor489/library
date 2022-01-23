df699f7fb512cff2929dde651b46a846d1c10c32
IGOR <you@example.com>
2022-01-23 15:44:48 +0300

    Lesson 2. Component Authors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AuthorList from './components/Author.js'
 
 class App extends React.Component {
   constructor(props) {
@@ -9,10 +10,31 @@ class App extends React.Component {
           'authors': []
       }
   }
+
+  componentDidMount() {
+      const authors = [
+          {
+              'first_name': 'Фёдор',
+              'last_name': 'Достоевский',
+              'birthday_year': 1821
+          },
+          {
+              'first_name': 'Александр',
+              'last_name': 'Грин',
+              'birthday_year': 1880
+          },
+      ]
+      this.setState(
+          {
+              'authors': authors
+          }
+      )
+  }
+
     render () {
     return (
         <div>
-          Main App
+            <AuthorList authors={this.state.authors} />
         </div>
     )
   }
