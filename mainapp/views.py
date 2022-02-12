41091887b29d2af67a9e5770e57cd0c74da0f942
IGOR <you@example.com>
2022-02-13 01:19:26 +0300

    Lesson 7. Mainapp

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
-# Create your views here.
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    serializer_class = AuthorSerializer
+    queryset = Author.objects.all()
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
