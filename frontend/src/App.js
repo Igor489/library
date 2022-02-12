d6f3477a94fa5d092920a653a91257c027422a76
IGOR <you@example.com>
2022-02-13 00:22:01 +0300

    Lesson 7. Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,106 +1,124 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import React from 'react'
+import AuthorList from './components/Author.js'
+import BookList from './components/Book.js'
+import AuthorBookList from './components/AuthorBook.js'
+import LoginForm from './components/Auth.js'
+import {BrowserRouter, Route, Switch, Redirect, Link} from 'react-router-dom'
 import axios from 'axios'
-import logo from './logo.svg';
-import './bootstrap/css/bootstrap.min.css'
-import './bootstrap/css/sticky-footer-navbar.css'
-import Footer from './components/Footer.js'
-import Navbar from './components/Menu.js'
-import UserList from './components/User.js'
-import {ProjectList, ProjectDetail} from './components/Project.js'
-import ToDoList from './components/ToDo.js'
+import Cookies from 'universal-cookie';
 
 
-
-const DOMAIN = 'http://127.0.0.1:8000'
-const get_url = (url) => `${DOMAIN}${url}`
-
-
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            navbarItems: [
-                {name: 'Users', href: '/'},
-                {name: 'Projects', href: '/projects'},
-                {name: 'TODOs', href: '/todos'},
-            ],
-            users: [],
-            projects: [],
-            project: {},
-            todos: []
-        }
-
-    //this.getProject = this.getProject.bind(this);
-    }
-
-    render() {
-         return (
-              <Router>
-                  <header>
-                    <Navbar navbarItems={this.state.navbarItems} />
-                  </header>
-                  <main role="main" class="flex-shrink-0">
-                      <div className="container">
-                        <Switch>
-                            <Route exact path='/'>
-                                <UserList users={this.state.users} />
-                            </Route>
-                            <Route exact path='/projects'>
-                                <ProjectList items={this.state.projects} />
-                            </Route>
-                            <Route exact path='/todos'>
-                                <ToDoList items={this.state.todos} />
-                            </Route>
-                            <Route path="/project/:id" children={<ProjectDetail getProject={(id) => this.getProject(id)} item={this.state.project} />} />
-                        </Switch>
-                      </div>
-                  </main>
-
-              <Footer />
-            </Router>
-
-
-            )
-    }
-
-    getProject(id) {
-        console.log('call')
-        console.log(get_url(`/api/projects/${id}`))
-        axios.get(get_url(`/api/projects/${id}`))
-        .then(response => {
-            console.log(response.data)
-            this.setState({project: response.data})
-        }).catch(error => console.log(error))
-    }
-
-
-
-    componentDidMount() {
-            axios.get(get_url('/api/users/'))
-        .then(response => {
-            //console.log(response.data)
-            this.setState({users: response.data.results})
-        }).catch(error => console.log(error))
-
-        axios.get(get_url('/api/projects/'))
-        .then(response => {
-            //console.log(response.data)
-            this.setState({projects: response.data.results})
-        }).catch(error => console.log(error))
-
-        axios.get(get_url('/api/todos/'))
-        .then(response => {
-            //console.log(response.data)
-            this.setState({todos: response.data.results})
-        }).catch(error => console.log(error))
-
-    }
+const NotFound404 = ({ location }) => {
+  return (
+    <div>
+        <h1>Страница по адресу '{location.pathname}' не найдена</h1>
+    </div>
+  )
 }
 
 
-export default App;
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      'authors': [],
+      'books': [],
+      'token': ''
+    }
+  }
+
+  set_token(token) {
+    const cookies = new Cookies()
+    cookies.set('token', token)
+    this.setState({'token': token}, ()=>this.load_data())
+  }
+
+  is_authenticated() {
+    return this.state.token != ''
+  }
+
+  logout() {
+    this.set_token('')
+  }
+
+  get_token_from_storage() {
+    const cookies = new Cookies()
+    const token = cookies.get('token')
+    this.setState({'token': token}, ()=>this.load_data())
+  }
+
+  get_token(username, password) {
+    axios.post('http://127.0.0.1:8000/api-token-auth/', {username: username, password: password})
+    .then(response => {
+        this.set_token(response.data['token'])
+    }).catch(error => alert('Неверный логин или пароль'))
+  }
+
+  get_headers() {
+    let headers = {
+      'Content-Type': 'application/json'
+    }
+  if (this.is_authenticated())
+    {
+        headers['Authorization'] = 'Token ' + this.state.token
+    }
+    return headers
+  }
+
+
+  load_data() {
+
+    const headers = this.get_headers()
+    axios.get('http://127.0.0.1:8000/api/authors/', {headers})
+        .then(response => {
+            this.setState({authors: response.data})
+        }).catch(error => console.log(error))
+
+    axios.get('http://127.0.0.1:8000/api/books/', {headers})
+        .then(response => {
+            this.setState({books: response.data})
+        }).catch(error => {
+          console.log(error)
+          this.setState({books: []})
+        })
+  }
+
+  componentDidMount() {
+    this.get_token_from_storage()
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <BrowserRouter>
+            <nav>
+              <ul>
+                <li>
+                  <Link to='/'>Authors</Link>
+                </li>
+                <li>
+                  <Link to='/books'>Books</Link>
+                </li>
+                <li>
+                    {this.is_authenticated() ? <button onClick={()=>this.logout()}>Logout</button> : <Link to='/login'>Login</Link>}
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route exact path='/' component={() => <AuthorList items={this.state.authors} />}  />
+              <Route exact path='/books' component={() => <BookList items={this.state.books} />} />
+              <Route exact path='/login' component={() => <LoginForm get_token={(username, password) => this.get_token(username, password)} />} />
+              <Route path="/author/:id">
+                <AuthorBookList items={this.state.books} />
+              </Route>
+              <Redirect from='/authors' to='/' />
+              <Route component={NotFound404} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+    )
+  }
+}
+
+export default App
