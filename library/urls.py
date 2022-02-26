e0bdb4708753cf4d019a4be1531a0c4896a5e744
IGOR <you@example.com>
2022-02-26 22:52:53 +0300

    Lesson 9. Api

--- library/urls.py
+++ library/urls.py
@@ -15,19 +15,64 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from rest_framework.authtoken import views
-from mainapp.views import AuthorViewSet, BookViewSet
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from userapp.views import UserViewSet
+from todoapp.views import ToDoViewSet, ProjectViewSet
+from rest_framework.schemas import get_schema_view as gsv
 
 router = routers.DefaultRouter()
 router.register('authors', AuthorViewSet)
 router.register('books', BookViewSet)
+router.register('users', UserViewSet)
+router.register('todos', ToDoViewSet)
+router.register('projects', ProjectViewSet)
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API documentation",
+      default_version='1.0',
+      # description="Test description",
+      # terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="admin@admin.local"),
+      # license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   # permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token)
-]
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
+    path('openapi', gsv(
+        title="TODOMONSTER",
+        description="API for all things …",
+        version="1.0"
+    ), name='openapi-schema'),
+
+    path('my-swagger-ui/', TemplateView.as_view(
+        template_name='todoapp/swagger.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
+
+    path('my-redoc/', TemplateView.as_view(
+        template_name='todoapp/redoc.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='redoc'),
+]
